bc72482b69466578a310be736f0d63027a68c5dd
julia <palant.ulia@gmail.com>
2020-04-10 20:26:29 +0300

    1
    Signed-off-by: julia <palant.ulia@gmail.com>

--- Task_2.py
+++ Task_2.py
@@ -4,8 +4,17 @@
 Используйте форматирование строк.
 '''
 
-time = input('Введите время в секундах: ')
+#print(type(time))
+while True:
+    time = input('Введите время в секундах: ')
+    if time.isdigit():
+        time = int(time)
+        break
+    else:
+        print('Введите целое число')
 
-
-result (f {hours} : {minutes} : {secondes})
+hours = time // 60 // 60
+seconds = time % 60
+minutes = time // 60 - hours * 60
+result = (f"{hours:>02}:{minutes:>02}:{seconds:>02}")
 print(result)
